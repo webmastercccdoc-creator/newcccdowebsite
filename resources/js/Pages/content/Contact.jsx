9af6209c9208d77cc4085e29be182770f0c18b10
webmasterccdoc-creator <webmastercccdoc@gmail.com>
2026-09-15 09:39:55 +0800

    website Update

--- resources/js/Pages/content/Contact.jsx
+++ resources/js/Pages/content/Contact.jsx
@@ -460,25 +460,37 @@ export default function Contact() {
                                 Send us a <span className="text-[#059669]">message</span>
                             </motion.h3>
 
-                            {/* Success Message */}
-                            <AnimatePresence>
-                                {formStatus.success && (
-                                    <motion.div
-                                        className="mb-4 p-4 bg-green-50 border border-green-200 rounded-lg text-green-700"
-                                        variants={successVariants}
-                                        initial="hidden"
-                                        animate="visible"
-                                        exit="exit"
-                                    >
-                                        <div className="flex items-center">
-                                            <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                                            </svg>
-                                            <span>Your message has been sent successfully!</span>
+                        {/* Success Message */}
+                        <AnimatePresence>
+                            {formStatus.success && (
+                                <motion.div
+                                    className="mb-4 p-4"
+                                    variants={successVariants}
+                                    initial="hidden"
+                                    animate="visible"
+                                    exit="exit"
+                                >
+                                    <div className="flex items-start">
+                                        <svg
+                                            className="w-5 h-5 mr-3 flex-shrink-0 text-[#059669] mt-0.5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        <div>
+                                            <p className="font-semibold text-[#059669]">
+                                                Message sent successfully!
+                                            </p>
                                         </div>
-                                    </motion.div>
-                                )}
-                            </AnimatePresence>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
 
                             {/* Error Message */}
                             {formStatus.error && (
